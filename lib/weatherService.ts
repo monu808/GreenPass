bfa1260a552b766ec4303aaedb36cd0407be7e7f
ShaikhWarsi <86195374+ShaikhWarsi@users.noreply.github.com>
2026-01-18 08:04:20 +0530

    Delete lib directory

--- lib/weatherService.ts
+++
@@ -1,7 +0,0 @@
-// Re-export the Tomorrow.io weather service as the main weather service
-export { weatherService, destinationCoordinates, TomorrowWeatherService } from './tomorrowWeatherService';
-export type { WeatherData, TomorrowApiResponse } from './tomorrowWeatherService';
-
-// For backward compatibility, also export as default
-import { weatherService } from './tomorrowWeatherService';
-export default weatherService;
